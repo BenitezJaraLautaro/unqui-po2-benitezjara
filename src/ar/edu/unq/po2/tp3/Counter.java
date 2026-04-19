c92ee2795b64502f6111055398e42deac20cb720
Lautaro Benitez Jara <lautaro.benitezjara76@gmail.com>
2026-04-19 13:44:30 -0300

    metodo para agregar numeros y contar pares

--- src/ar/edu/unq/po2/tp3/Counter.java
+++ src/ar/edu/unq/po2/tp3/Counter.java
@@ -3,4 +3,35 @@ import java.util.ArrayList;
 
 public class Counter {
 	private ArrayList<Integer> colector; 
+	
+	public ArrayList<Integer> getColector() {
+		return colector;
+	}
+
+	public void setColector(ArrayList<Integer> colector) {
+		this.colector = colector;
+	}
+	
+	
+	public Counter() {
+		super();
+		this.colector = new ArrayList<>();
+	}
+	
+	public void addNumber(Integer numero) {
+		this.colector.add(numero); 
+	}
+	
+	public int cuantosParHay() {
+		int colector2 = 0;
+		for (int numero: colector) {
+			if (numero % 2 == 0) {
+				colector2 = colector2 + 1;
+			}
+		}
+		return colector2;
+	}
+	
+	
+	
 }
